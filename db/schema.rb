af918ce673c5c8566598452e017920e856e3ca06
Ignacio N. Lucero Ascencio <igwisnirrol@gmail.com>
2015-04-11 22:03:03 +0200

    Scaffold User

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140707111715) do
+ActiveRecord::Schema.define(version: 20150411194917) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.integer  "karma"
+    t.text     "about"
+    t.string   "submitted"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "widgets", force: true do |t|
     t.string   "name"
