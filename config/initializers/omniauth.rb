c160961b1e7b63e5f76a6bdb03eff6af1db65043
Ignacio N. Lucero Ascencio <igwisnirrol@gmail.com>
2015-05-06 01:06:18 +0200

    Auth stuff

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,5 +3,5 @@ GOOGLE_KEY = ENV['GOOGLE_KEY']
 GOOGLE_SECRET = ENV['GOOGLE_SECRET']
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, GOOGLE_KEY, GOOGLE_SECRET, {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+  provider :google_oauth2, '658212163004-saj6lsb4pprpgirvfvk1l692vmd1kqj3.apps.googleusercontent.com', 'oHRkXhE0mLN6MrfMuEmXfdZA', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
